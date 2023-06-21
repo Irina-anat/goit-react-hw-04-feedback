78947499997c5b8a050470b76bce2628fedd1d35
Iryna Rozmaita <iriszechka@ukr.net>
2023-06-21 20:18:01 +0300

    update useState, useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import Section from 'components/Section';
 import Statistics from 'components/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions';
 import Notification from 'components/Notification';
 import css from './App.module.css';
 
-export class App extends React.Component {
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const addFeedbackOption = option => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1
+    }));
+  };
+
+  const { good, neutral, bad } = feedback;
+
+  const totalFeedback = good + neutral + bad;
+  const positivePercentage = totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
+
+  useEffect(() => {
+    // Виконується дія при зміні стану feedback
+
+  }, [feedback]);
+
+  return (
+    <div className={css.container}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={addFeedbackOption}
+        />
+      </Section>
+
+      {totalFeedback ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      ) : (
+        <Notification message={"There is no feedback"} />
+      )}
+    </div>
+  );
+};
+
+export { App };
+          
+
+
+
+
+
+
+
+
+
+
+/*export class App extends React.Component {
   state = {
     good: 0,
     neutral: 0,
@@ -54,5 +116,5 @@ export class App extends React.Component {
               : (<Notification message={"There is no feedback"} />)}
     
             </div>)}
-};
+};*/
 
